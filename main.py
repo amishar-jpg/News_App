43e9cacc5c7dfb572f63978d295d3b044b304a51
amishar-jpg <amisha_r@ce.iitr.ac.in>
2025-12-27 22:13:07 +0530

    changes made

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
+from config import api_key
 
-api_key = "d2904786182146a5a7657b22bf013a94"
 query = "technology"
 
 url = f"https://newsapi.org/v2/everything?q={query}&from=2025-11-27&sortBy=publishedAt&apiKey={api_key}"
